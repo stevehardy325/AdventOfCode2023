c706d4c5dc3cb9412b0be95737dee5d34a32d577
Steve Hardy <stevehardy325@gmail.com>
2023-12-08 02:17:58 -0500

    12/8 cleanup some pylint complaints

--- 08/run2.py
+++ 08/run2.py
@@ -9,9 +9,6 @@ import re
 TEST_FNAME = 'test3.txt'
 INPUT_FNAME = 'input.txt'
 
-NODE_DCT = {}
-cycle_directions = ''
-
 ################################
 #############  Unit tests
 ################################
@@ -38,10 +35,6 @@ class TestAnswerMethod(unittest.TestCase):
         # validation result for testing goes here
         self.assertEqual(test_answer, 6)
 
-    '''def testExampleAnswer2(self):
-        test_answer =get_answer(TEST_FNAME2)
-        # validation result for testing goes here
-        self.assertEqual(test_answer, 6)'''
 
 ################################
 #############  Data functions
@@ -68,14 +61,13 @@ def get_dataset_lines(filename):
 
 
 
-@cache
-def stepsToNextZ(start_node: str, cycle_position: int):
+def stepsToNextZ(start_node: str, node_dct: dict, cycle_directions: str, cycle_position: int):
     cur_node = start_node
     additional_steps = 0
 
     while additional_steps == 0 or cur_node[2] != 'Z':
         go_direction = cycle_directions[(cycle_position + additional_steps) % len(cycle_directions)]
-        cur_node = NODE_DCT[cur_node][go_direction]
+        cur_node = node_dct[cur_node][go_direction]
         additional_steps += 1
 
     return additional_steps, cur_node
@@ -90,23 +82,21 @@ def get_answer_optimized(inputfile):
 
     data_list = get_dataset_lines(inputfile)
 
-    global cycle_directions
     cycle_directions = data_list[0].strip()
     links = data_list[2:]
 
-    global NODE_DCT
-    NODE_DCT = {}
+    node_dct = {}
 
     for link_str in links:
         start, lr_dct = getNodes(link_str)
-        NODE_DCT[start] = lr_dct
+        node_dct[start] = lr_dct
 
-    cur_nodes = [n for n in NODE_DCT.keys() if n[2] == 'A']
+    cur_nodes = [n for n in node_dct.keys() if n[2] == 'A']
 
     z_steps = [0 for n in cur_nodes]
 
     for i, node in enumerate(cur_nodes):
-        steps_to_z_ending, _ = stepsToNextZ(node, 0)
+        steps_to_z_ending, _ = stepsToNextZ(node, node_dct, cycle_directions, 0)
         z_steps[i] = steps_to_z_ending
 
 
